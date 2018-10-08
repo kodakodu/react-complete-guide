197bf1413cd470fb99ccc6e497dd62d7928101bd
kodakodu <kodakodu@gmail.com>
2018-10-08 09:24:30 +0200

    Section 3 - Session 45

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 29 },
+        { name: event.target.value, age: 28 },
+        {name : 'Stephanie', age: 27}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,7 +45,8 @@ class App extends Component {
         <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}>My hobbies: Racing</Person>
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My hobbies: Racing</Person>
         <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
     <div>
     <p onClick={props.click}>I'm {props.name} and i'm {props.age} year(s) old !</p>
     <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name}/>
     </div>
     )
 }
