9f674e48beb26ee7623a058070d91047e93edcc4
kodakodu <kodakodu@gmail.com>
2018-10-21 11:31:45 +0200

    Section 4 - Session 51

--- src/App.js
+++ src/App.js
@@ -46,7 +46,26 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
-    
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+        <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+        <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+        <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+      </div>
+      );
+    }
+
     return (
       <div className="App">
       <h1>REACT Application</h1>
@@ -54,24 +73,8 @@ class App extends Component {
         <p>This is really working!</p>
         <button
           style={style} 
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { 
-          this.state.showPersons ? 
-            <div>
-              <Person 
-                  name={this.state.persons[0].name} 
-                  age={this.state.persons[0].age}/>
-              <Person 
-                  name={this.state.persons[1].name} 
-                  age={this.state.persons[1].age}
-                  click={this.switchNameHandler.bind(this, 'Max!')}
-                  changed={this.nameChangedHandler}>My hobbies: Racing</Person>
-              <Person 
-                  name={this.state.persons[2].name} 
-                  age={this.state.persons[2].age}/>
-            </div> : null
-        }
-
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>    
+        {persons}
       </div>
     );
   }
